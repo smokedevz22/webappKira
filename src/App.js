8d29224e6b6573e137a9e623ef347a217df82b1f
unknown <smokedev.z@gmail.com>
2020-12-07 02:04:18 -0300

    base web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 import './App.css';
+import Amplify, { Auth } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AmplifySignOut />
+    My App
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
